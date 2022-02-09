caef793b30ab542ee9dbccfe03080301e6536bc6
stefanom13 <stefanomancuso13@gmail.com>
2022-02-09 16:42:23 +0100

    inserimento gioco mail

--- js/app.js
+++ js/app.js
@@ -1,8 +1,45 @@
 // Mail
-// Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
-// stampa un messaggio appropriato sull’esito del controllo.
 
+// Chiedi all’utente il suo nome
+    let nameUser = prompt('benvenuto, inserisci il tuo nome')
+
+    // Chiedi all’utente la sua email
+    let emailUser = prompt('inserisci la tua mail');
+
+    console.log (nameUser, emailUser);
+
+    const listEmail = [
+        "pippo@gmail.it",
+         "plutp@gmail.it",
+         "lupo@gmail.it",
+         "michele@gmail.it",
+         "topo@gmail.it",
+         "ciao"
+    ];
+
+        console.log(listEmail);
+
+  // creo varabiabile per vedere effetivamente se email è corretta
+    let checkMail = false;
+  // controlla che sia nella lista di chi può accedere,
+
+    for (let i = 0; i < listEmail.length; i++) {
+        const element = listEmail[i];
+         // stampa un messaggio appropriato sull’esito del controllo.
+        if (emailUser === element) {
+
+           checkMail = true;
+        }
+
+    }
+    // stampa un messaggio appropriato sull’esito del controllo.
+    if (checkMail === true ){
+        alert('ok puoi entrare')
+
+    }else {
+        alert('mi dispiace accesso negato')
+    }
+    
 
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
