a2e2dd95ab453388ad93ea1855085f97c5381bc6
stefanom13 <stefanomancuso13@gmail.com>
2022-02-09 17:20:08 +0100

    inserimento gioco dadi

--- js/app.js
+++ js/app.js
@@ -1,46 +1,64 @@
-// Mail
+// Gioco dei dadi
+// dichiarare variabili 
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+
+// let numberPlayer = Math.floor ( Math.random () * 6 + 1 );
+// let numberPc = Math.floor ( Math.random () * 6 + 1 );
+
+// console.log(numberPlayer, numberPc)
+
+// Stabilire il vincitore, in base a chi ha il numero più alto.  (e se pari?)
+
+// if (numberPlayer > numberPc) {
+//     alert(`Il tuo numero è: ${numberPlayer}. Il numero del pc è: ${numberPc}. Hai vinto!`)    
+// } else   {
+
+//     alert(`Il tuo numero è: ${numberPlayer}. Il numero del pc è: ${numberPc}. Hai perso!`)
+
+// }
+// if (numberPlayer === numberPc) {
+//     alert(`Il tuo numero è: ${numberPlayer}. Il numero del pc è: ${numberPc}. pareggio!`)    
+// } 
+ 
+
+// gioco email 
 
 // Chiedi all’utente il suo nome
-    let nameUser = prompt('benvenuto, inserisci il tuo nome')
+let nameUser = prompt('benvenuto, inserisci il tuo nome')
 
-    // Chiedi all’utente la sua email
-    let emailUser = prompt('inserisci la tua mail');
+// Chiedi all’utente la sua email
+let emailUser = prompt('inserisci la tua mail');
 
-    console.log (nameUser, emailUser);
+console.log (nameUser, emailUser);
 
-    const listEmail = [
-        "pippo@gmail.it",
-         "plutp@gmail.it",
-         "lupo@gmail.it",
-         "michele@gmail.it",
-         "topo@gmail.it",
-         "ciao"
-    ];
+const listEmail = [
+    "pippo@gmail.it",
+     "plutp@gmail.it",
+     "lupo@gmail.it",
+     "michele@gmail.it",
+     "topo@gmail.it",
+     "ciao"
+];
 
-        console.log(listEmail);
+    console.log(listEmail);
 
-  // creo varabiabile per vedere effetivamente se email è corretta
-    let checkMail = false;
-  // controlla che sia nella lista di chi può accedere,
+// creo varabiabile per vedere effetivamente se email è corretta
+let checkMail = false;
+// controlla che sia nella lista di chi può accedere,
 
-    for (let i = 0; i < listEmail.length; i++) {
-        const element = listEmail[i];
-         // stampa un messaggio appropriato sull’esito del controllo.
-        if (emailUser === element) {
+for (let i = 0; i < listEmail.length; i++) {
+    const element = listEmail[i];
+     // stampa un messaggio appropriato sull’esito del controllo.
+    if (emailUser === element) {
 
-           checkMail = true;
-        }
-
+       checkMail = true;
     }
-    // stampa un messaggio appropriato sull’esito del controllo.
-    if (checkMail === true ){
-        alert('ok puoi entrare')
 
-    }else {
-        alert('mi dispiace accesso negato')
-    }
-    
+}
+// stampa un messaggio appropriato sull’esito del controllo.
+if (checkMail === true ){
+    alert('ok puoi entrare')
 
-// Gioco dei dadi
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi ha il numero più alto.  (e se pari?)
+}else {
+    alert('mi dispiace accesso negato')
+}
